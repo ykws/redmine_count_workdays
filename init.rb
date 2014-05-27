74ab6007f99a2f355d116c117059bbc5e5cd9803
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2014-05-27 11:22:55 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_count_workdays do
+  name 'Redmine Count Workdays plugin'
+  author 'Yoshiyuki Kawashima'
+  description 'Show count work days and over work hours'
+  version '0.0.1'
+  url 'https://github.com/ykws/redmine_count_workdays'
+  author_url 'https://github.com/ykws'
+end
